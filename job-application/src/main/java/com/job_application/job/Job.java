d87ca7b1e13f69033613512e9a1f47ea55b7e929
Shantanu Patel <shantu.patel@gmail.com>
2024-10-20 11:13:54 +0530

    Job entity class file created.

---
+++ job-application/src/main/java/com/job_application/job/Job.java
@@ -0,0 +1,85 @@
+package com.job_application.job;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Job {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String description;
+	private BigDecimal minSalary;
+	private BigDecimal maxSalary;
+	private String location;
+
+	public Job() {
+		super();
+	}
+
+	public Job(Long id, String name, String description, BigDecimal minSalary, BigDecimal maxSalary, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.location = location;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getMinSalary() {
+		return minSalary;
+	}
+
+	public void setMinSalary(BigDecimal minSalary) {
+		this.minSalary = minSalary;
+	}
+
+	public BigDecimal getMaxSalary() {
+		return maxSalary;
+	}
+
+	public void setMaxSalary(BigDecimal maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
